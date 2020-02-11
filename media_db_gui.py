378a259c58a4bfb2cbb0d564774beb0de0e57206
Morgan Butryn <mbutryn@debian02.CIS.COM>
2020-02-11 10:03:36 -0600

    modified:   media_db_gui.py

--- media_db_gui.py
+++ media_db_gui.py
@@ -4,23 +4,61 @@
 """GUI version of media_db"""
 import pickle
 import tkinter as tk
-from tkinter import scrolledtext
+from tkinter import messagebox
+from tkinter.scrolledtext import ScrolledText
 
-class MainMenu(tk.Frame):
+"""class MainMenu(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
         
-        lbl_title = tk.Label(text="Media Database", font=("Verdana", "20"))
-        lbl_title.grid(row = 0, column = 0, sticky = "news")
+        self.columnconfigure(0, weight = 1)
+        self.columnconfigure(1, weight = 1)
+        self.columnconfigure(2, weight = 1)
+        
+        
+        lbl_title = tk.Label(text="Media database", font=("Verdana", "20"))
+        lbl_title.grid(row = 0, column = 1, columnspan = 3, sticky = "news")
         
         btn_add = tk.Button(text="Add", font=("Verdana", "14"))
-        btn_add.grid(row = 1, column = 0)
+        btn_add.grid(row = 1, column = 1)
+        
         btn_edit = tk.Button(text="Edit", font=("Verdana", "14"))
-        btn_edit.grid(row = 2, column = 0)
+        btn_edit.grid(row = 2, column = 1)
+        
         btn_remove = tk.Button(text="Remove", font=("Verdana", "14"))
-        btn_remove.grid(row = 3, column = 0)
+        btn_remove.grid(row = 3, column = 1)
+        
+        btn_search = tk.Button(text="Search", font=("Verdana", "14"))
+        btn_search.grid(row = 4, column = 1)  
+        
         btn_save = tk.Button(text="Save", font=("Verdana", "14"))
-        btn_save.grid(row = 4, column = 0)        
+        btn_save.grid(row = 5, column = 1)"""
+        
+"""class EditMenu(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)"""
+        
+        
+
+class SearchMenu(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        
+        lbl_title = tk.Label(text="Search", font=("Verdana", "20"))
+        lbl_title.grid(row = 0, column = 1, sticky = "news")
+        
+        lbl_searchby = tk.Label(text="Search by:", font=("Verdana", "14"))
+        lbl_searchby.grid(row = 1, column = 0, sticky = "news")
+        
+        lbl_searchfor = tk.Label(text="Search for:", font=("Verdana", "14"))
+        lbl_searchfor.grid(row = 3, column = 0, sticky = "news")
+        
+        self.searchlist = {"genre", "title"}
+        self.searchselect = ""
+
+        dbx_search = tk.OptionMenu(self.searchselect, *self.searchlist)
+        dbx_search.grid(row = 2, column = 0, sticky = "news")
+
 #main
 if __name__ == "__main__":
     picklefile = open("games.pickle", "rb")
@@ -30,6 +68,6 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("400x400")
     root.title("Media Library")
-    main_menu = MainMenu()
+    main_menu = SearchMenu()
     main_menu.grid(row = 0, column = 0, sticky = "news")
     root.mainloop()
